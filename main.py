0643a1a42c4b245e57dc91205b53bdf14bbbce5d
shaikhshehzad <shehzadali3113@gmail.com>
2023-12-25 15:02:31 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
